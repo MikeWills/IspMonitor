e5e1bf0d0d21b4bcf9e7aaebe22a82c0dee36258
Mike Wills <mike@mikewills.me>
2015-03-02 23:33:03 -0600

    Moved the config into a separate file

---
+++ conf-setup.php
@@ -0,0 +1,10 @@
+<?php 
+$apiKey = ""; /*replace with your apiKey*/
+$monitorID  = 11111; /*replace with your monitorID*/
+
+$dbServer = "localhost";
+$dbName = "pingmon"; /* Enter your DB name */
+$dbUser = ""; /* Enter your DB username */
+$dbPassword = ""; /* Enter your DB password */
+$serviceName = ""; /* This allows you to track multiple services. */
+?>
--- loadping.php
+++ loadping.php
@@ -1,15 +1,7 @@
 <?php
-/*Configuration - Start*/
-$apiKey     = ""; /* Replace with your apiKey */
-$monitorID  = 111111; /* Replace with your monitorID */
-$url    = "https://api.uptimerobot.com/getMonitors?apiKey=" . $apiKey . "&monitors=" . $monitorID . "&format=xml&responseTimes=1";
+require("config.php");
 
-$dbServer = "localhost"; /* Enter your DB host */
-$dbName = ""; /* Enter your DB name */
-$dbUser = ""; /* Enter your DB username */
-$dbPassword = ""; /* Enter your DB password */
-$service = ""; /* This allows you to track multiple services. */
-/*Configuration - End*/
+$url = "https://api.uptimerobot.com/getMonitors?apiKey=" . $apiKey . "&monitors=" . $monitorID . "&format=xml&responseTimes=1";
  
 /*Curl Request - Start*/
 $c = curl_init($url);
@@ -23,23 +15,20 @@ $xml = simplexml_load_string($responseXML);
 $count = 0;
 
 foreach($xml->monitor as $monitor) {
-
 	foreach ($monitor->responsetime as $responsetime) {
-
 		if (intval($responsetime['value']) != 0){
-			
 			mysql_connect($dbServer, $dbUser, $dbPassword) or die(mysql_error());
 			mysql_select_db($dbName) or die(mysql_error());
 
 			$result = mysql_query("SELECT DateTime FROM PingLog WHERE Service = '" . $serviceName . 
-				"' AND DateTime = '" . $responsetime['datetime'] . "'") or die(mysql_error());
+				"' AND DateTime = '" . date("Y-m-d H:i:s", strtotime($responsetime['datetime'])) . "'") or die(mysql_error());
 
 			if (mysql_num_rows($result) == 0){
-				$sql = "INSERT INTO PingLog (DateTime, PingTime, Service) VALUES('" . $responsetime['datetime'] . 
+				$sql = "INSERT INTO PingLog (DateTime, PingTime, Service) VALUES('" . date("Y-m-d H:i:s", strtotime($responsetime['datetime'])) . 
 					"', " . intval($responsetime['value']) . ", '" . $serviceName . "')";
 				mysql_query($sql) or die(mysql_error());	
 
-				echo "Date: " . $responsetime['datetime'] . " | ";
+				echo "Date: " . date("Y-m-d H:i:s", strtotime($responsetime['datetime'])) . " | ";
 				echo "Response time: " . $responsetime['value'] . " ms \n";
 				$count++;
 			}
